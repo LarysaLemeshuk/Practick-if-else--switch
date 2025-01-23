1a0c16d1d28753ac6bc40e64e0355547597b254a
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-01-23 16:23:25 +0100

    practicke

--- index.js
+++ index.js
@@ -1,67 +1,54 @@
-//Задача 1
+/*
+1. Створити об’єкт user з наступними властивостями:
+ім'я, 
+прізвище, 
+електронна адреса, 
+password,
+номер телефона,
+адреса (є рядком або *об'єктом з властивостями місто, вулиця, дім, квартира) ; 
+*/
 
-let input1 = prompt("Введіть перше число");
-let input2 = prompt("Введіть друге число");
-let number1 = Number(input1);
-let number2 = Number(input2);
-
-let sum = number1 + number2;
-
-if (sum % 5 === 0) {
-  alert("Сума кратна 5");
-} else {
-  //  alert("Сума не кратна 5");
-}
-
-//Задача 2
-
-function calculatorArea (Length){
-  return length * length;
-}
-let length = 5;
-console.log();
-
-//Задача 3
-function drink() {
-  let choice = prompt("Оберіть напій:\n1 - Чай\n2 - Кава\n3 - Сік\n4 - Вода");
-  choice = Number(choice);
-
-  switch (choice) {
-    case 1:
-      alert("Ви обрали чай");
-      break;
-    case 2:
-      alert("Ви обрали каву");
-      break;
-    case 3:
-      alert("Ви обрали сік");
-      break;
-    case 4:
-      alert("Ви обрали воду");
-      break;
-    default:
-      alert("Такого напою у нас нема");
+const user = {
+  firstName: 'Larysa',
+  lastName: 'Lemeshuk',
+  mail: 'Larysalemeshuk@gmail.com',
+  password: '12345QWEERR',
+  phoneNumber: '123456789',
+  address: {
+    city: 'Kovel',
+    street: 'Kotova',
+    house: 18,
+    apartment: 5
   }
+};
+
+console.log(user);
+
+/*
+2. Написати функцію, яка приймає об’єкт юзера і виводить вітання 
+типу “Доброго дня, (ім’я_юзера) (прізвище юзера)”
+*/
+
+function greetUser(user){
+  console.log(`Hello, ${user.firstName} ${lastName}`);
 }
+greetUser(user);
 
-drink();
+/*
+3. Написати функцію, яка приймає об’єкт і виводить в консоль
+ всі його властивості (за допомогою циклу for..in)
+ */
 
-// Задача 4
-
-let purchaseAmount = prompt('Введіть суму покупки:');
-purchaseAmount = Number(purchaseAmount);
-let discount = purchaseAmount > 800 ? 5 : (purchaseAmount > 500 ? 3 : 0);
-let finalAmount = purchaseAmount - (purchaseAmount * discount / 100);
-
-//Задача 5
-
-function divideWithoutRemainder(a, b) {
-  return a % b === 0;
-}
-let num1 = 10;
-let num2 = 5;
-divideWithoutRemainder (num1, num2);
-
-
-
-
+ /*
+ 4. Створити об’єкт car (машина) з властивостями:
+колір
+модель
+марка
+об’єм двигуна
+місткість (можлива кількість пасажирів)
+поточна швидкість (speed)
+максимальна можлива швидкість (maxSpeed) та методами:
+accelerate (збільшення швидкості): метод приймає кількість кілометрів/годину (ціле число) та додає до поточної швидкості (speed), але результат не має перевищувати максимально можливої швидкості для цієї машини
+deaccelerate (зменшення швидкості): метод приймає ціле число (кількість кілометрів на годину) і зменшує поточну швидкість (speed) на вказану, але результат не може бути від’ємним
+stop (зупинка) - метод нічого не приймає, при виклику встановлює поточну швидкість (speed) рівною 0
+*/
