b191fab09d0c9b27a2696a802c40683ddd7fe5fa
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-03-05 19:38:43 +0100

    JS цикли

--- index.js
+++ index.js
@@ -1,105 +1,76 @@
-/*
-1. Створити об’єкт user з наступними властивостями:
-ім'я, 
-прізвище, 
-електронна адреса, 
-password,
-номер телефона,
-адреса (є рядком або *об'єктом з властивостями місто, вулиця, дім, квартира) ; 
-*/
+// 1. Вивести в консоль квадрати чисел від 1 до 10 (за допомогою циклу)
 
-const user = {
-  firstName: "Larysa",
-  lastName: "Lemeshuk",
-  mail: "Larysalemeshuk@gmail.com",
-  password: "12345QWEERR",
-  phoneNumber: "123456789",
-  address: {
-    city: "Kovel",
-    street: "Kotova",
-    house: 18,
-    apartment: 5,
-  },
-};
-
-console.log(user);
-
-/*
-2. Написати функцію, яка приймає об’єкт юзера і виводить вітання 
-типу “Доброго дня, (ім’я_юзера) (прізвище юзера)”
-*/
-
-function greetUser(user) {
-  console.log(`Hello, ${user.firstName} ${lastName}`);
-}
-greetUser(user);
-
-/*
-3. Написати функцію, яка приймає об’єкт і виводить в консоль
- всі його властивості (за допомогою циклу for..in)
- */
-
-function userObj(user) {
-  for (let key in user) {
-    console.log(`${key}: ${user[key]}`);
-  }
+for (let i = 1; i <= 10; i++) {
+    console.log(i * i);
 }
 
-/*
- 4. Створити об’єкт car (машина) з властивостями:
-колір
-модель
-марка
-об’єм двигуна
-місткість (можлива кількість пасажирів)
-поточна швидкість (speed)
-максимальна можлива швидкість (maxSpeed) та методами:
-accelerate (збільшення швидкості): метод приймає кількість кілометрів/годину (ціле число) та додає до поточної швидкості (speed), але результат не має перевищувати максимально можливої швидкості для цієї машини
-deaccelerate (зменшення швидкості): метод приймає ціле число (кількість кілометрів на годину) і зменшує поточну швидкість (speed) на вказану, але результат не може бути від’ємним
-stop (зупинка) - метод нічого не приймає, при виклику встановлює поточну швидкість (speed) рівною 0
-*/
 
-const car = {
-  color: "red",
-  model: "Toyota",
-  brand: "Camry",
-  engineVolume: 2.5,
-  capacity: 5,
-  speed: 0,
-  maxSpeed: 200,
+//2. Написати функцію, яка запитує через prompt пароль користувача, порівнює його з тим, який у неї збережений (захардкоджений у константі).
+// Якщо пароль співпав - виводить на консоль “Ви успішно увійшли в систему”, якщо ні - продовжує запитувати пароль і перевіряти, поки не введуть правильно (за допомогою вічного цикла)
 
-  accelerate(kmh) {
-    this.speed += kmh;
-    if (this.speed > this.maxSpeed) {
-      this.speed = this.maxSpeed;
+function checkPassword() {
+    const CORRECT_PASSWORD = "12345"; 
+    let input;
+    
+    while (true) {
+        input = prompt("Введіть пароль:");
+        if (input === CORRECT_PASSWORD) {
+            console.log("Ви успішно увійшли в систему");
+            break;
+        }
     }
-  },
-  deaccelerate(kmh) {
-    this.speed -= kmh;
-    if (this.speed < 0) {
-      this.speed = 0;
-    }
-  },
-  stop() {
-    this.speed = 0;
-  },
-};
-
-/*
-5. На основі об’єкта з завдання №4 розробити функцію-конструктор для масового створення таких об’єктів за переданими параметрами. 
-В якості перевірки створити декілька екземплярів таких об’єктів з різними властивостями
-*/
-
-function Car(color, model, brand, engineVolume, capacity, maxSpeed) {
-  this.color = color;
-  this.model = model;
-  this.brand = brand;
-  this.engineVolume = engineVolume;
-  this.capacity = capacity;
-  this.speed = 0;
-  this.maxSpeed = maxSpeed;
 }
 
-const car1 = new Car("red", "Toyota", "Camry", 3, 5, 200);
-const car2 = new Car("black", "Jaguar", "E-paces", 2, 4, 250);
-const car3 = new Car("white", "Mercedes-Benx", "190 sl", 1.9, 2, 180);
+checkPassword();
+
+
+
+// 3. Обчислити сумму чисел від 1 до 100 і вивести її в консоль
+
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+    sum += i;
+}
+console.log("Сума чисел від 1 до 100:", sum);
+
+
+
+// 4. Вивести числа від 10 до 50, які кратні 5.
+
+for (let i = 10; i <= 50; i += 5) {
+    console.log(i);
+}
+
+
+
+// 5. Намалювати в консолі символами “*” трикутник заданого користувачем розміру
+
+// function drawTriangle(size) {
+//     for (let i = 1; i <= size; i++) {
+//         console.log("*".repeat(i));
+//     }
+// }
+
+// let size = parseInt(prompt("Введіть розмір трикутника:"));
+// drawTriangle(size);
+
+ 
+// 6. Намалювати пустий квадрат з діагоналлю (зліва зверху - до права низу)
+
+function squareWithDiagonal(size) {
+    for (let i = 0; i < size; i++) {
+        let row = '';
+        for (let j = 0; j < size; j++) {
+            if (i === 0 || i === size - 1 || j === 0 || j === size - 1 || i === j) {
+                row += '*';
+            } else {
+                row += '';
+            }
+        }
+        console.log(row);
+    }
+}
+
+let squareSize = parseInt(prompt('Введіть розмір квадрата'));
+
+squareWithDiagonal(squareSize);
