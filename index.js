aeb85daddcc068bb306448360f74426fedf66b0b
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-04-10 17:39:20 +0200

    DOM. Створення DOM-елементів

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// Завдання 1 
+// Завдання 1
 const section = document.createElement('section');
 section.setAttribute('id', 'root');
 section.textContent = 'Це створений section.';
 section.style.color = 'green';
 document.body.appendChild(section);
 
-// Завдання 2 
+// Завдання 2
 const existingElement = document.getElementById('existingElement');
 
-  existingElement.style.backgroundColor = 'purple';
-  existingElement.style.color = 'white';
+existingElement.style.backgroundColor = 'purple';
+existingElement.style.color = 'white';
 
-// Завдання 3 
+// Завдання 3
 const userName = prompt('Введіть ваше ім’я:');
 
-  const greeting = document.createElement('h1');
-  greeting.textContent = `Вітаю, ${userName}`;
-  //document.body.appendChild(greeting);
+const greeting = document.createElement('h1');
+greeting.textContent = `Вітаю, ${userName}`;
+document.body.appendChild(greeting);
